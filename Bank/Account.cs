86accb7b37565dfd88453a7d071629517fda4a19
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2025-07-18 20:25:53 -0300

    Constructor, overloading and encapsulation

---
+++ Bank/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+        public Account(int number, string holder)
+        {
+            Number = number;
+            Holder = holder;
+        }
+
+        public Account(int number, string holder, double initialDeposit) : this(number, holder)
+        {
+            Deposit(initialDeposit);
+        }
+
+        public bool Deposit(double amount)
+        {
+            if (amount > 0.0)
+            {
+                Balance += amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Withdrawal(double amount)
+        {
+            if (amount > 0.0 && Balance >= amount)
+            {
+                Balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Number: " + Number
+                + ", Holder: " + Holder
+                + ", Balance: $ " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount;
+            Account account;
+
+            Console.Write("Enter the account number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Enter the account holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Would you like to make an initial deposit (y/n)? ");
+            char answer = char.Parse(Console.ReadLine());
+            if (answer == 'y' || answer == 'Y')
+            {
+                Console.Write("Enter the initial deposit amount: ");
+                amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new Account(number, holder, amount);
+            }
+            else
+            {
+                account = new Account(number, holder);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account details:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter the deposit amount: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (account.Deposit(amount))
+            {
+                Console.WriteLine("Deposit successful");
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount");
+            }
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.Write("Enter the amount to withdraw: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (account.Withdrawal(amount))
+            {
+                Console.WriteLine("Withdrawal successful");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance or invalid amount");
+            }
+            Console.WriteLine("Updated account data:");
+            Console.WriteLine(account);
+        }
+    }
+}
